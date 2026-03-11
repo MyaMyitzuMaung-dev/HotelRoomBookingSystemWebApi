e0e4dab016ff10b23db65702617b61797d018b36
EmiriMaung <poelay1995@gmail.com>
2026-03-11 12:33:18 +0630

    feat: Report

--- IPB2.HotelRoomServiceWebApi/Controllers/ReportController.cs
+++ IPB2.HotelRoomServiceWebApi/Controllers/ReportController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IPB2.HotelRoomService.Database.AppDbContextModels;
+using IPB2.HotelRoomServiceWebApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IPB2.HotelRoomServiceWebApi.Controllers
 {
@@ -7,5 +10,45 @@ namespace IPB2.HotelRoomServiceWebApi.Controllers
     [ApiController]
     public class ReportController : ControllerBase
     {
+        private readonly AppDbContext _context;
+
+        public ReportController(AppDbContext context)
+        {
+            _context = context;
+        }
+        // Booking Report
+        [HttpGet("booking-report")]
+        public async Task<IActionResult> GetBookingReport(int pageNo = 1, int pageSize = 10)
+        {
+            var totalCount = await _context.Bookings
+                .Where(x => !x.IsDeleted)
+                .CountAsync();
+
+            var report = await _context.Bookings
+                .Where(x => !x.IsDeleted)
+                .Include(x => x.Room)
+                .Include(x => x.Guest)
+                .Skip((pageNo - 1) * pageSize)
+                .Take(pageSize)
+                .Select(x => new BookingReportDto
+                {
+                    BookingId = x.BookingId,
+                    RoomNumber = x.Room!.RoomNumber,
+                    GuestName = x.Guest!.FirstName + " " + x.Guest.LastName,
+                    CheckInDate = x.CheckInDate,
+                    CheckOutDate = x.CheckOutDate,
+                    Status = x.Status,
+                    TotalAmount = x.TotalAmount
+                })
+                .ToListAsync();
+
+            return Ok(new
+            {
+                pageNo,
+                pageSize,
+                totalCount,
+                data = report
+            });
+        }
     }
 }
---
+++ IPB2.HotelRoomServiceWebApi/Models/BookingReportDto.cs
@@ -0,0 +1,19 @@
+﻿namespace IPB2.HotelRoomServiceWebApi.Models
+{
+    public class BookingReportDto
+    {
+        public int BookingId { get; set; }
+
+        public string? RoomNumber { get; set; }
+
+        public string? GuestName { get; set; }
+
+        public DateOnly? CheckInDate { get; set; }
+
+        public DateOnly? CheckOutDate { get; set; }
+
+        public string? Status { get; set; }
+
+        public decimal? TotalAmount { get; set; }
+    }
+}
